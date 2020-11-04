6e4fd44c3a0ec61c011231d0009ce6ef4304b8e3
David Richter <d.richter@quadient.com>
2020-11-04 11:42:23 +0100

    incremental task

--- buildSrc/src/main/kotlin/ReverseFiles.kt
+++ buildSrc/src/main/kotlin/ReverseFiles.kt
@@ -1,9 +1,13 @@
 import org.gradle.api.DefaultTask
 import org.gradle.api.file.DirectoryProperty
+import org.gradle.api.file.FileType
 import org.gradle.api.file.ProjectLayout
 import org.gradle.api.file.RegularFileProperty
 import org.gradle.api.model.ObjectFactory
 import org.gradle.api.tasks.*
+import org.gradle.work.ChangeType
+import org.gradle.work.Incremental
+import org.gradle.work.InputChanges
 import org.gradle.workers.WorkAction
 import org.gradle.workers.WorkParameters
 import org.gradle.workers.WorkerExecutor
@@ -17,6 +21,7 @@ abstract class ReverseFiles @Inject constructor(
 ): DefaultTask() {
 
     @InputDirectory
+    @Incremental
     @PathSensitive(PathSensitivity.RELATIVE)
     val sourceDirectory: DirectoryProperty = objects.directoryProperty().convention(projectLayout.projectDirectory.dir("src"))
 
@@ -24,12 +29,20 @@ abstract class ReverseFiles @Inject constructor(
     val outputDirectory: DirectoryProperty = objects.directoryProperty().convention(projectLayout.buildDirectory.dir("reversed"))
 
     @TaskAction
-    fun reverse() {
+    fun reverse(inputChanges: InputChanges) {
         var worker = workerExecutor.noIsolation()
-        sourceDirectory.get().asFile.listFiles()!!.forEach { input ->
-            worker.submit(ReverseFileAction::class.java) {
-                it.inputFile.set(input)
-                it.outputFile.set(outputDirectory.file(input.name))
+        inputChanges.getFileChanges(sourceDirectory).forEach { change ->
+            if (change.fileType == FileType.DIRECTORY) return@forEach
+            val outputFile = outputDirectory.file(change.file.name).get().asFile
+            if (change.changeType == ChangeType.REMOVED) {
+                logger.lifecycle("Deleting file: $outputFile")
+                outputFile.delete()
+            } else {
+                logger.lifecycle("Reversing file: ${change.file} to $outputFile")
+                worker.submit(ReverseFileAction::class.java) {
+                    it.inputFile.set(change.file)
+                    it.outputFile.set(outputFile)
+                }
             }
         }
     }
